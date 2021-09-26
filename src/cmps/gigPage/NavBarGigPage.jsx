f7316d65eb8bea618519d8526d9410c3d935745a
Omer Morag <omermorag2@gmail.com>
2021-09-26 20:50:07 +0300

    omer commit

--- src/cmps/gigPage/NavBarGigPage.jsx
+++ src/cmps/gigPage/NavBarGigPage.jsx
@@ -3,25 +3,20 @@ import { Link } from 'react-router-dom';
 
 export function NavBarGigPage(props) {
   return (
-    <div className="gig-review">
-      <nav>
-        <ul>
-          <li className="nav-overview">
+    <div className="navbar-gigpage" >
+      <nav className="navbar-links">
+          <div className="nav-overview">
             <a href="#overview">Overview</a>
-          </li>
-          <li className="nav-description">
-            <a href="#reviews">Reviews</a>
-          </li>
-          <li className="nav-aboutSeller">
+          </div>
+          <div className="nav-aboutSeller">
             <a href="#aboutSeller">About The Seller</a>
-          </li>
-          <li className="nav-packegeTable">
-            <a href="#aboutSeller">About The Seller</a>
-          </li>
-          <li className="nav-recommendations">
-            <a href="#recommendations">Recommendations</a>
-          </li>
-        </ul>
+          </div>
+          <div className="nav-packegeTable">
+            <a href="#aboutSeller">Order Details</a>
+          </div>
+          <div className="nav-recommendations">
+            <a href="#recommendations">Reviews</a>
+          </div>
       </nav>
       <button className="far fa-heart"></button>
     </div>
--- src/pages/GigPage.jsx
+++ src/pages/GigPage.jsx
@@ -16,39 +16,61 @@ export class GigPage extends React.Component {
     const { gig } = this.state;
     return (
       <div className="gig-page-body">
-        <NavBarGigPage gig={gig} />
-        <h1>
-          I will fix search console, semrush, ahrefs, moz, errors to rank higher
-        </h1>
+        <div className="gig-page-container">
+          <div className="container-navbar">
+          <NavBarGigPage gig={gig} />
+          </div>
+          <div  id="overview" className="gig-review-comtainer">
+            <h1>
+              I will fix search console, semrush, ahrefs, moz, errors to rank
+              higher
+            </h1>
 
-        <div className="seller-overview">
-          <img
-            src="https://www.tzomet-hrz.co.il/wp-content/uploads/2019/12/1318c8c679a76a7dc58fd67c2714fe54-e1576169232593.jpg"
-            className="profile-pict-img"
-            alt="bibi"
-          />
-          <p>jeffreymsmith</p>
-          <StarRate reviews={null} />
-          <p>(16223)</p>
+            <div className="seller-overview">
+              <div>
+                <img
+                  src="https://www.tzomet-hrz.co.il/wp-content/uploads/2019/12/1318c8c679a76a7dc58fd67c2714fe54-e1576169232593.jpg"
+                  className="profile-pict-img"
+                  alt="bibi"
+                />
+              </div>
+              <p className="seller-name">jeffreymsmith</p>
+              <StarRate reviews={null} />
+              <p>(16223)</p>
+            </div>
 
-          <img
-            src="https://microlancer.lancerassets.com/v2/services/b3/25f9956bab41098547a961166d740a/service_card_Simple_logos-02.jpg"
-            className="gig-images"
-            alt=""
-          />
+            <div className="gig-photos">
+              <img
+                src="https://microlancer.lancerassets.com/v2/services/b3/25f9956bab41098547a961166d740a/service_card_Simple_logos-02.jpg"
+                className="gig-images"
+                alt=""
+              />
+            </div>
 
-          <h2>About This Gig</h2>
-          <p>{` am a Professional voice over actor with a In House Vocal Booth and I would love to record whatever you need me to, up to 100 words, for $5!* I will deliver any voiceover less than 24 hours of your order. ONE DAY DELIVERY! NEED IT FAST? Purchase the VIP Express Delivery to get your order next! TV, For Video YouTube Video Voiceover Match audio to video Voice Over Online projects Phone greetings On hold Message Radio Commercials Internet Programs any projects Business`}</p>
-          <h2>About This Seller</h2>
-          <img
-            src="https://www.tzomet-hrz.co.il/wp-content/uploads/2019/12/1318c8c679a76a7dc58fd67c2714fe54-e1576169232593.jpg"
-            className="profile-pict-img"
-            alt="bibi"
-          />
-          <p>jeffreymsmith</p>
-          <StarRate reviews={null} />
-          <p>(16223)</p>
-        </div>
+            <div className="gallery"></div>
+
+            <h2>About This Gig</h2>
+            <p className="gig-description">{` am a Professional voice over actor with a In House Vocal Booth and I would love to record whatever you need me to, up to 100 words, for $5!* I will deliver any voiceover less than 24 hours of your order. ONE DAY DELIVERY! NEED IT FAST? Purchase the VIP Express Delivery to get your order next! TV, For Video YouTube Video Voiceover Match audio to video Voice Over Online projects Phone greetings On hold Message Radio Commercials Internet Programs any projects Business`}</p>
+
+            <h2>About This Seller</h2>
+            <div className="about-seller">
+              <div className="about-the-seller-img">
+                <img
+                  src="https://www.tzomet-hrz.co.il/wp-content/uploads/2019/12/1318c8c679a76a7dc58fd67c2714fe54-e1576169232593.jpg"
+                  className="profile-pict-img"
+                  alt="bibi"
+                />
+              </div>
+              <div  id="aboutSeller"className="about-the-seller-details">
+                <p>jeffreymsmith</p>
+                <p>Responsive Web Designer And WordPress Expert</p>
+                <StarRate reviews={null} />
+                <p>(16223)</p>
+                <button className="contact-the-seller">Contact Me</button>
+              </div>
+            </div>
+            </div>
+          </div>
       </div>
     );
   }
