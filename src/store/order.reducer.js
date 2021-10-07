a9dbabfd49215cd08e897760ca4b3bf00e45c980
ShaulBatzon <shaulbatzon1@gmail.com>
2021-10-07 17:32:48 +0300

    update

--- src/store/order.reducer.js
+++ src/store/order.reducer.js
@@ -25,24 +25,4 @@ export function orderReducer(state = initialState, action = {}) {
     default:
       return state
   }
-  
-  export function orderReducer(state = initialState, action = {}) {
-    switch (action.type) {
-      case 'SET_ORDERS':
-        return { ...state, orders: action.userOrders }
-      case 'ADD_ORDER':
-        console.log("from action omer",action.orders);
-        return { ...state, orders: [...state.orders, action.orders] }
-      case 'REMOVE_ORDER':
-        return { ...state, orders: state.orders.filter(order => order._id !== action.ordersId) }
-      case 'UPDATE_ORDER':
-        return {
-          ...state,
-          orders: state.orders.map(order =>
-            order._id === action.order._id ? action.order : order
-          )}
-      default:
-        return state
-    }
-  }
-  
+}
