fe36aa7a724edc2706951eb2331e7a3fdb2b1df4
Omer Morag <omermorag2@gmail.com>
2021-10-07 17:35:15 +0300

    push 2

--- src/store/order.reducer.js
+++ src/store/order.reducer.js
@@ -25,28 +25,4 @@ export function orderReducer(state = initialState, action = {}) {
     default:
       return state
   }
-<<<<<<< HEAD
-  
-  export function orderReducer(state = initialState, action = {}) {
-    switch (action.type) {
-      case 'SET_ORDERS':
-        return { ...state, orders: action.userOrders }
-      case 'ADD_ORDER':
-        console.log("from action omer",action.orders);
-        return { ...state, orders: [...state.orders, action.orders] }
-      case 'REMOVE_ORDER':
-        return { ...state, orders: state.orders.filter(order => order._id !== action.ordersId) }
-      case 'UPDATE_ORDER':
-        return {
-          ...state,
-          orders: state.orders.map(order =>
-            order._id === action.order._id ? action.order : order
-          )}
-      default:
-        return state
-    }
-  }
-  
-=======
 }
->>>>>>> 38d41f059050ded6a0aa7c452a2c96235ab7ae8f
